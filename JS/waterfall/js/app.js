660c90c6d63d2a87d05e324d5ce6268849b236f6
guazipi <sam_lhx@126.com>
2016-04-27 21:18:18 +0800

    waterfall done

--- JS/waterfall/js/app.js
+++ JS/waterfall/js/app.js
@@ -3,14 +3,79 @@
  */
 window.onload = function () {
     imgLocation("container", "box");
+    var imgData = {"data": [
+        {"src": "2.jpg"},
+        {"src": "3.jpg"},
+        {"src": "4.jpg"},
+        {"src": "5.jpg"},
+        {"src": "6.jpg"},
+        {"src": "7.jpg"},
+        {"src": "8.jpg"},
+        {"src": "9.jpg"},
+    ]};
+    window.onscroll = function () {
+        if (checkFlag("container", "box")) {
+            var cparent = document.getElementById("container");
+            for(var i=0;i<imgData.data.length;i++){
+                var ccontent=document.createElement("div");
+                ccontent.className="box";
+                cparent.appendChild(ccontent);
+                var boximg=document.createElement("div");
+                boximg.className="box_img";
+                ccontent.appendChild(boximg);
+                var img=document.createElement("img");
+                img.src="./images/"+imgData.data[i].src;
+                boximg.appendChild(img);
+            }
+            imgLocation("container", "box");
+        }
+    }
+}
+
+function checkFlag(parent, content) {
+    var cparent = document.getElementById(parent);
+    var ccontent = getChildElement(cparent, content);
+    var lastContentHeight = ccontent[ccontent.length - 1].offsetTop;
+
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var pageHeight = document.documentElement.clientHeight || document.body.clientHeight;
+    //console.log(lastContentHeight+" "+scrollTop+" "+pageHeight);
+    if (lastContentHeight < scrollTop + pageHeight) {
+        return true;
+    }
+
 }
 function imgLocation(parent, content) {
     var cparent = document.getElementById(parent);
     var ccontent = getChildElement(cparent, content);
-    console.log(ccontent);
-    var imgWidth=content[0].offsetWidth;
-    var cols=Math.floor(document.documentElement.clientWidth/imgWidth);
+    var imgWidth = ccontent[0].offsetWidth;
+    var cols = Math.floor(document.documentElement.clientWidth / imgWidth);
+    cparent.style.cssText = "width:" + imgWidth * cols + "px;margin:0 auto;";
 
+    var boxHeightArr = [];
+    for (var i = 0; i < ccontent.length; i++) {
+        if (i < cols) {
+            boxHeightArr.push(ccontent[i].offsetHeight);
+            //console.log(ccontent[i].offsetHeight);
+        } else {
+            var minheight = Math.min.apply(null, boxHeightArr);
+            var minIndex = getminheightLocation(boxHeightArr, minheight);
+
+            ccontent[i].style.position = 'absolute';
+            ccontent[i].style.top = minheight + "px";
+            ccontent[i].style.left = ccontent[minIndex].offsetLeft + "px";
+
+            boxHeightArr[minIndex] = boxHeightArr[minIndex] + ccontent[i].offsetHeight;
+        }
+    }
+}
+
+function getminheightLocation(boxHeightArr, minHeight) {
+    for (var i in boxHeightArr) {
+        if (boxHeightArr[i] == minHeight) {
+            return i;
+        }
+    }
 }
 function getChildElement(parent, content) {
     var contentArr = [];
